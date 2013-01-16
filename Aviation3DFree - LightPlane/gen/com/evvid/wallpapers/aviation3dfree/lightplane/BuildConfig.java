99a8ab1d9bb42fb20fe1b470b137fa86e81323a4
David Trounstine <david@evvid.com>
2013-01-15 16:42:14 -0800

    Removed nested gitignore files

---
+++ Aviation3DFree - LightPlane/gen/com/evvid/wallpapers/aviation3dfree/lightplane/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.evvid.wallpapers.aviation3dfree.lightplane;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ IslandNights/gen/com/evvid/wallpapers/islandnights/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.evvid.wallpapers.islandnights;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
