a79b15196cbc948b9034520e0bb59ef9a3eece22
David Trounstine <david@evvid.com>
2013-02-23 08:32:18 -0800

    .gitignore repair

--- Aviation3D - Helo/gen/com/evvid/wallpapers/aviation3d/helo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.evvid.wallpapers.aviation3d.helo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
